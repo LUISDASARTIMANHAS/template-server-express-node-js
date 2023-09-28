dc6c8df92ece7c39059700e03807a4fef7c039f2
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-09-27 23:01:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 const cors = require("cors");
+const { exec } = require('child_process');
 
 const filesServer = __dirname + "/src/";
 const path_pages = filesServer + "pages/";

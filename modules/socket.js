7fc07d37fa146cf80c6f4891325fa7b7130d5593
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-01-31 15:46:43 -0300

    updated bugs

--- modules/socket.js
+++ modules/socket.js
@@ -6,29 +6,34 @@ const app = express();
 const router = express.Router();
 const wsInstance = expressWs(app);
 const configs = JSON.parse(fs.readFileSync("config.json", "utf8"));
+const wsConfig = configs.wsSystem;
 
-const port = configs.portWS || 255;
+const port = wsConfig.portWS || 2255;
 const files = __dirname + "/src/";
 const path_pages = files + "pages/";
 const forbiddenFilePath = path.join(path_pages, "forbidden.html");
 const notFoundFilePath = path.join(path_pages, "not-found.html");
 
-// Adicione a rota para WebSocket (HTTP)
-const wsModule = app.ws('/ws', (ws, req) => {
-    console.log('WS SERVER: Cliente conectado via WebSocket.');
+if (wsConfig.enabled) {
+    // Adicione a rota para WebSocket (HTTP)
+    const wsModule = app.ws('/ws', (ws, req) => {
+        console.log('WS SERVER: Cliente conectado via WebSocket.');
 
-    ws.on('message', (message) => {
-        console.log(`Recebido: ${message}`);
-        ws.send(`WS SERVER: Recebido: ${message}`);
+        ws.on('message', (message) => {
+            console.log(`Recebido: ${message}`);
+            ws.send(`WS SERVER: Recebido: ${message}`);
+        });
+
+        ws.on('close', () => {
+            console.log('WS SERVER: Conexão fechada.');
+        });
     });
 
-    ws.on('close', () => {
-        console.log('WS SERVER: Conexão fechada.');
+    app.listen(port, () => {
+        console.log(`Servidor Websocket(Ws) rodando na porta: ${port}`);
     });
-});
 
-app.listen(port, () => {
-    console.log(`Servidor Websocket(Ws) rodando na porta: ${port}`);
-});
+    module.exports = wsModule;
+}
 
-module.exports = wsModule;
+module.exports = router

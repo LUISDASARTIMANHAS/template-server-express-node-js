385d05327cf153c3de146ce8a755946f7a43865e
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-04-06 11:48:45 -0300

    updated bugs

--- modules/fetchModule.js
+++ modules/fetchModule.js
@@ -1,75 +1,86 @@
 const fetch = require("node-fetch");
 const headersDefault = {
-  "Content-Type": "application/json",
   "x-forwarded-proto": "https,http,http",
   "x-forwarded-port": "443,80,80",
   "accept-encoding": "gzip",
 };
 
-async function fetchGet(url, header, callback) {
-  try {
-    const newHeaders = Object.assign(headersDefault, header);
-    const requestOptions = {
-      method: "GET",
-      headers: newHeaders,
-    };
-    console.log("FETCH GET", url);
-    const response = await fetch(url, requestOptions);
-    const contentType = response.headers.get("content-type");
-    
-    // Verifica se houve erro na resposta
-    if (!response.ok) {
-      throw new Error(`${response.status} ${response.statusText}`);
-    }
-
-    // Verifica o tipo de conteúdo retornado
-    if (contentType && contentType.includes("application/json")) {
-      // Se for JSON, retorna o JSON
-      const jsonData = await response.json();
-      callback(null, jsonData);
-    } else {
-      // Se não for JSON, retorna o conteúdo como texto
-      const textData = await response.text();
-      callback(null, textData);
-    }
-  } catch (error) {
-    console.error("Erro ao fazer a requisição: ", error);
-    callback(error, null);
-  }
+function fetchGet(url, header, callback) {
+  const newHeaders = Object.assign(headersDefault, header);
+  const requestOptions = {
+    method: "GET",
+    headers: newHeaders,
+  };
+  console.log("FETCH GET", url);
+  fetch(url, requestOptions)
+    .then((response) => {
+      const contentType = response.headers.get("content-type");
+      // Verifica se houve erro na resposta
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      // Verifica o tipo de conteúdo retornado
+      if (contentType && contentType.includes("application/json")) {
+        // Se for JSON, retorna o JSON
+        return response.json();
+      } else {
+        // Se não for JSON, retorna o conteúdo como texto
+        return response.text();
+      }
+    })
+    .then((data) => {
+      console.log("FETCH GET RECEBIDO! OK 200");
+      callback(null, data);
+    })
+    .catch((error) => {
+      console.error(`Erro ao fazer a requisição para ${url}: ${error}`);
+      callback(error, null);
+    });
 }
 
-
-async function fetchPost(url, payload, header, callback) {
-  try {
-    const newHeaders = Object.assign(headersDefault, header);
-    const requestOptions = {
-      method: "POST",
-      headers: newHeaders,
-      body: JSON.stringify(payload),
-    };
-    console.log("FETCH POST", url);
-    const response = await fetch(url, requestOptions);
-    const contentType = response.headers.get("content-type");
-    
-    // Verifica se houve erro na resposta
-    if (!response.ok) {
-      throw new Error(`${response.status} ${response.statusText}`);
+function fetchPost(url, payload, header, callback) {
+  const defaultContentType = {
+    "content-type": `application/json; charset=UTF-8`,
+  };
+  var newHeaders = headersDefault;
+  newHeaders = Object.assign(headersDefault,header || defaultContentType);
+  const requestOptions = {
+    method: "POST",
+    headers: newHeaders,
+    body: payload,
+  };
+  
+  if(newHeaders['content-type'] == "application/json; charset=UTF-8"){
+        console.log("Convertendo payload para json!");
+        payload = JSON.stringify(payload);
     }
-
-    // Verifica o tipo de conteúdo retornado
-    if (contentType && contentType.includes("application/json")) {
-      // Se for JSON, retorna o JSON
-      const jsonData = await response.json();
-      callback(null, jsonData);
-    } else {
-      // Se não for JSON, retorna o conteúdo como texto
-      const textData = await response.text();
-      callback(null, textData);
-    }
-  } catch (error) {
-    console.error("Erro ao fazer a requisição:", error);
-    callback(error, null);
-  }
+  
+  console.log("FETCH POST", url);
+  fetch(url, requestOptions)
+    .then((response) => {
+      const contentType = response.headers.get("content-type");
+      // Verifica se houve erro na resposta
+      if (!response.ok) {
+        console.error(response);
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      // Verifica o tipo de conteúdo retornado
+      if (contentType && contentType.includes("application/json")) {
+        // Se for JSON, retorna o JSON
+        return response.json();
+      } else {
+        // Se não for JSON, retorna o conteúdo como texto
+        return response.text();
+      }
+    })
+    .then((data) => {
+      console.log("FETCH POST ENVIADO! OK 200");
+      callback(null, data);
+    })
+    .catch((error) => {
+      console.error(`Erro ao fazer a requisição para ${url}: ${error}`);
+      callback(error, null);
+    });
 }
 
-module.exports = {fetchGet,fetchPost}
+module.exports = { fetchGet, fetchPost };
